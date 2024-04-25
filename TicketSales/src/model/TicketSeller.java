14432c71549eddc744a7331f6c267ba8625723ec
lunerave <lunerave07@naver.com>
2024-04-26 01:41:30 +0900

    Code Implementation : model TicketSeller

---
+++ TicketSales/src/model/TicketSeller.java
@@ -0,0 +1,13 @@
+package model;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
