e3a40d1f4cbfb5584cb49c78b3a014e0ffa5d7f3
lunerave <lunerave07@naver.com>
2024-04-28 21:15:09 +0900

    Code Implementation: TicketSeller, Audience encapsulation for lower coupling

--- TicketSales/src/Main.java
+++ TicketSales/src/Main.java
@@ -8,13 +8,15 @@ public class Main {
         Ticket ticket2 = new Ticket();
         Ticket ticket3 = new Ticket();
 
-        ticket1.setFee(50L);
-        ticket2.setFee(100L);
+        ticket1.setFee(70L);
+        ticket2.setFee(200L);
+        ticket3.setFee(100L);
 
         System.out.println("ticket1 = " + ticket1.getFee());
         System.out.println("ticket2 = " + ticket2.getFee());
+        System.out.println("ticket3 = " + ticket3.getFee());
 
-        TicketOffice ticketOffice = new TicketOffice(0L, ticket1, ticket2);
+        TicketOffice ticketOffice = new TicketOffice(0L, ticket1, ticket2, ticket3);
         TicketSeller ticketSeller = new TicketSeller(ticketOffice);
         Theater theater = new Theater(ticketSeller);
 
@@ -26,6 +28,11 @@ public class Main {
         Bag hasInvitationBag = new Bag(new Invitation(), 100L);
         Audience hasInvitationAudience = new Audience(hasInvitationBag);
         theater.enter(hasInvitationAudience);
+        System.out.println("ticketOffice sales= " + ticketOffice.getAmount());
+
+        Bag dontHaveInvitationBag2 = new Bag(100L);
+        Audience dontHaveInvitationAudience2 = new Audience(dontHaveInvitationBag2);
+        theater.enter(dontHaveInvitationAudience2);
         System.out.println("ticketOffice sales= " + ticketOffice.getAmount());
     }
 }
